36fd3922f3c0deb6400d481be0daae32a5e35558
Calen McCann <43736265+CalenMac@users.noreply.github.com>
2019-07-17 10:57:15 -0400

    Create Temboo.h

---
+++ Temboo.h
@@ -0,0 +1,7 @@
+#define TEMBOO_ACCOUNT ""          // your Temboo account name 
+#define TEMBOO_APP_KEY_NAME ""     // your Temboo app key name
+#define TEMBOO_APP_KEY  ""         // your Temboo app key
+#define TWILIO_ACCOUNT_SID ""      // the Account SID from your Twilio account
+#define TWILIO_AUTH_TOKEN ""       // the Auth Token from your Twilio account
+#define TWILIO_NUMBER ""           
+#define RECIPIENT_NUMBER "" 
